81e1adefaccea982071041b40df44a07768c840d
omerf <omerf@DESKTOP-VCO142Q>
2025-06-07 17:59:56 +0300

    Add NonNull annotation to CORS configuration

--- src/main/java/com/medipol/javaFinal/JavaFinalApplication.java
+++ src/main/java/com/medipol/javaFinal/JavaFinalApplication.java
@@ -2,6 +2,10 @@ package com.medipol.javaFinal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.lang.NonNull;
 
 @SpringBootApplication
 public class JavaFinalApplication {
@@ -10,4 +14,16 @@ public class JavaFinalApplication {
 		SpringApplication.run(JavaFinalApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(@NonNull CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+						.allowedHeaders("*");
+			}
+		};
+	}
 }
